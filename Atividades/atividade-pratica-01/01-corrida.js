605e55f7be30be26052b839c9d8e53620125d5e0
marcospsc <70164888+marcospsc@users.noreply.github.com>
2020-09-14 11:24:39 -0300

    atividade imc sem formatar

---
+++ Atividades/atividade-pratica-01/01-corrida.js
@@ -0,0 +1,6 @@
+
+var largada = document.querySelector("#adicionarCompetidor");
+
+function largada(){
+    
+}
--- Atividades/atividade-pratica-01/02-imc.js
+++ Atividades/atividade-pratica-01/02-imc.js
@@ -1,7 +1,7 @@
 function validar(campo) {
     let n = campo.value;
 
-    if (n.lenght == 0 || isNaN (parseInt(n))) {
+    if (n.lenght == 0 || isNaN(parseInt(n))) {
         window.alert("Informe o valor corretamente!");
         campo.value = "";
         campo.focus();
@@ -16,9 +16,35 @@ function calcularIMC() {
     let altura = document.dados.altura;
 
     if (validar(peso) && validar(altura)) {
-        let resultado = (parseInt(peso.value) / (parseInt(altura.value) * (parseInt(altura.value))));
-        document.dados.resultado.value = resultado * 10000;
+        let resultado = (parseInt(peso.value) / (parseInt(altura.value) * (parseInt(altura.value)))) * 10000;
+        
+        document.dados.resultado.value = resultado.toFixed(2);
     }
 
+    imprimeDiagnostico(document.dados.resultado.value);
 
+}
+
+function imprimeDiagnostico(resultado){
+
+    resultado = this.resultado.value;
+    
+    if (resultado < 18.5) {
+        window.alert("Indivíduo apresenta subnutrição - " + resultado);
+    }
+    else if (18.5 <= resultado && resultado < 24.9) {
+        window.alert("Indivíduo apresenta peso saudável - " + resultado);
+    }
+    else if (25.0 <= resultado && resultado < 29.9) {
+        window.alert("Indivíduo apresenta sobrepeso - " + resultado);
+    }
+    else if (30.0 <= resultado && resultado < 34.9) {
+        window.alert("Indivíduo apresenta obesidade grau 1 - " + resultado);
+    }
+    else if (35.0 <= resultado && resultado < 39.9) {
+        window.alert("Indivíduo apresenta obesidade grau 2 - " + resultado);
+    }
+    else {
+        window.alert("Indivíduo apresenta obesidade grau 3 - " + resultado);
+    }
 }
